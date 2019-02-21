df3a17df1005ad1760a1fc6d0fcdc40003c59b3e
xanendorf <xanendorf@gmail.com>
2019-02-20 20:36:01 -0600

    Pep8 Styling

--- App/model.py
+++ App/model.py
@@ -40,19 +40,18 @@ import pandas as pd
 import matplotlib
 
 
-
 def make_pipeline(NUMBER_OF_DOCS, istesting):
     doclist = []
     names = []
-    Path_Input = input("Enter a relative path, or hit enter for ../Data/Inputs:   ")
+    Path_Input = input(
+        "Enter a relative path, or hit enter for ../Data/Inputs:   ")
     if Path_Input == "":
         pathlist = Path("../Data/Inputs").glob('**/*.txt')
-    else: 
+    else:
         pathlist = Path(Path_Input).glob('**/*.txt')
 
     try:
         for path in tqdm(pathlist):
-        # because path is object not string
             path_in_str = str(path)
             name = path_in_str.split("\\")[3].split(".")[0]
             names.append(name.replace("_", " "))
@@ -69,9 +68,7 @@ def make_pipeline(NUMBER_OF_DOCS, istesting):
     except ValueError as I:
         print(I, "Did you enter a correct path?")
         sys.exit()
-
-
-    if istesting == True:
+    if istesting is True:
         print('Test set generated')
         doclist = doclist[NUMBER_OF_DOCS:NUMBER_OF_DOCS * 2]
         names = names[NUMBER_OF_DOCS: NUMBER_OF_DOCS * 2]
@@ -85,10 +82,7 @@ def make_pipeline(NUMBER_OF_DOCS, istesting):
         print(names[:5])
     if len(names) > 10:
         print('......', names[-5:])
-
-    
     return doclist, names
-
 
 
 def transform_tokens(doclist):
@@ -112,7 +106,6 @@ def transform_filtered(token_list, doclist, names):
     Stopwords_list = stopwords.words(
         'english') + more_stops + punc + maybe_bad_stops
     filtered_tokens = []
-    #WE CAN RUN 1 or many docs at once#
     names_list = []
     if len(names) > 1:
         if len(token_list) != len(doclist):
@@ -129,7 +122,8 @@ def transform_filtered(token_list, doclist, names):
                     #                 NNP proper noun, singular ‘Harrison’
                     #                 NNPS proper noun, plural ‘Americans’
                     if token not in Stopwords_list:
-                        if pos_tag(token) != 'NNP' and pos_tag(token) != 'NNPS':
+                        if pos_tag(token) != 'NNP' and pos_tag(
+                          token) != 'NNPS':
                             filtered_docs.append(token.lower())
                         else:
                             filtered_docs.append('proper_noun')
@@ -144,7 +138,8 @@ def transform_filtered(token_list, doclist, names):
                     #                 NNP proper noun, singular ‘Harrison’
                     #                 NNPS proper noun, plural ‘Americans’
                     if token not in Stopwords_list:
-                        if pos_tag(token) != 'NNP' and pos_tag(token) != 'NNPS':
+                        if pos_tag(token) != 'NNP' and pos_tag(
+                          token) != 'NNPS':
                             filtered_docs.append(token.lower())
                         else:
                             filtered_docs.append('proper_noun')
@@ -165,63 +160,58 @@ def transform_stemming(filtered_tokens):
 def transform_vectorize(stemmed, smallest_ngram, largest_ngram):
 
     vectorizer = TfidfVectorizer(stop_words=Stopwords_list,
-                                 ngram_range=(smallest_ngram, largest_ngram), max_df=0.55, min_df=0.01)
+                                 ngram_range=(
+                                     smallest_ngram, largest_ngram),
+                                 max_df=0.55, min_df=0.01)
 #     vectorizer = CountVectorizer(stop_words=Stopwords_list,
-#                                  ngram_range=(smallest_ngram, largest_ngram), max_df=0.75, min_df=0.01)
+#                                  ngram_range=(
+#                                   smallest_ngram, largest_ngram),
+#                                    max_df=0.75, min_df=0.01)
     sparseMatrix = vectorizer.fit_transform(stemmed)
     return sparseMatrix, vectorizer
 
 
-
-
-
 def GridSearch():
     from sklearn.model_selection import GridSearchCV
-    model = KMeans(init='k-means++', random_state=42, n_init=15
-                       )
-    param_grid = {'max_iter': [10, 50, 100, 150, 200, 250, 300, 350, 400, 500, 1000],
-                 'n_clusters': [25,30, 33, 35],
-                 }
+    model = KMeans(init='k-means++', random_state=42, n_init=15)
+    param_grid = {'max_iter': [10, 50, 100, 150, 200,
+                               250, 300, 350, 400, 500, 1000],
+                  'n_clusters': [25, 30, 33, 35]}
     grid = GridSearchCV(model, param_grid, verbose=3, n_jobs=8)
     grid.fit(sparseMatrix)
-
     lids = model.cluster_centers_
-
     score = model.score(sparseMatrix)
-    silhouette_score = metrics.silhouette_score(sparseMatrix, labels, metric='euclidean')
+    silhouette_score = metrics.silhouette_score(
+        sparseMatrix, labels, metric='euclidean')
     print(grid.best_params_)
     print(grid.best_score_)
 
     return grid, model, score, silhouette_score
 
 
-
-
 def estimator_cluster(sparseMatrix, vectorizer):
-    truek = 35  # FROM GRID SEARCH
+    truek = 35
     model = KMeans(n_clusters=truek, init='k-means++',
                    max_iter=50, n_init=1, random_state=42,
                    )
     model.fit(sparseMatrix)
-
     model_time = datetime.now().strftime("%b%d-%I%M%p")
     joblib.dump(model,  f'../Data/Outputs/s2s{model_time}.pkl')
     joblib.dump(vectorizer,  f'../Data/Outputs/vec{model_time}.pkl')
     order_centroids = model.cluster_centers_.argsort()[:, ::-1]
     terms = vectorizer.get_feature_names()
-
     print(f"Model Generated at {model_time}")
-
     labels = model.labels_
     centroids = model.cluster_centers_
     print("Cluster id labels for inputted data")
     print(labels)
     print("Centroids data")
-    #print (centroids)
+    print(centroids)
 
     kmeans_score = model.score(sparseMatrix)
     print("Score (Opposite of the value of X on the K-means objective, \n",
-          "which is Sum of distances of samples to their closest cluster center):")
+          "which is Sum of distances of samples to their closest cluster",
+          " center):")
     print(kmeans_score)
 
     silhouette_score = metrics.silhouette_score(
@@ -230,115 +220,24 @@ def estimator_cluster(sparseMatrix, vectorizer):
     print("Silhouette_score: ")
     print(silhouette_score)
 
-    return terms, order_centroids, model, truek, model_time, kmeans_score, silhouette_score
-
-
-
-
-
-
-
-
-
-
-# # SUMMARIZATION OF CORPORATE RISK FACTOR DISCLOSURE THROUGH TOPIC MODELING by Bao, Datta
-# strings = [
-#     'Topic 0: investment, property, distribution, interest, agreement',
-#     'Topic 1: regulation, change, law, financial, operation, tax, accounting ',
-#     'Topic 2: gas, price, oil, natural, operation, production Input prices risks ',
-#     'Topic 3: stock, price, share, market, future, dividend, security, stakeholder ',
-#     'Topic 4: cost, regulation, environmental, law, operation, liability',
-#     'Topic 5: control, financial, internal, loss, reporting, history ',
-#     'Topic 6: financial, litigation, operation, condition, action, legal, liability, regulatory, claim, lawsuit'
-#     'Topic 7: competitive, industry, competition, highly',
-#     'Topic 8: cost, operation, labor, operating, employee, increase, acquisition ',
-#     'Topic 9: product, candidate, development, approval, clinical, regulatory',
-#     'Topic 10: tax, income, asset, net, goodwill, loss, distribution, impairment, intangible ',
-#     'Topic 11: interest, director, officer, trust, combination, share, conflict ',
-#     'Topic 12: product, liability, claim, market, insurance, sale, revenue Potential defects in products',
-#     'Topic 13: loan, real, estate, investment, property, market, loss, portfolio ',
-#     'Topic 14: personnel, key, retain, attract, management, employee ',
-#     'Topic 15: stock, price, operating, stockholder, fluctuate, interest, volatile  ',
-#     'Topic 16: acquisition, growth, future, operation, additional, capital, strategy ',
-#     'Topic 17: condition, economic, financial, market, industry, change, affected, downturn, demand Macroeconomic risks ',
-#     'Topic 18: system, service, information, failure, product, operation, software, network, breach, interruption Disruption of operations'
-#     'Topic 19: cost, contract, operation, plan, increase, pension, delay',
-#     'Topic 20: customer, product, revenue, sale, supplier, relationship, key, portion, contract, manufacturing, rely Rely on few large customers',
-#     'Topic 21: property, intellectual, protect, proprietary, technology, patent, protection, harm',
-#     'Topic 22: product, market, service, change, sale, demand, successfully, technology, competition Volatile demand and results',
-#     'Topic 23: provision, law, control, change, stock, prevent, stockholder, Delaware, charter, delay, bylaw',
-#     'Topic 24: regulation, government, change, revenue, contract, law, service',
-#     'Topic 25: capital, credit, financial, market, cost, operation, rating, access, liquidity, downgrade ',
-#     'Topic 26: debt, indebtedness, cash, obligation, financial, credit, ',
-#     'Topic 27: operation, international, foreign, currency, rate, fluctuation',
-#     'Topic 28: loss, insurance, financial, loan, reserve, operation, cover',
-#     'Topic 29: operation, natural, facility, disaster, event, terrorist, weather ']
-# topics = [topic.split(":")[1] for topic in strings]
-
-
-# targets = {
-#     "Shareholder’s interest risk": topics[0],
-#     "Regulation changes(accounting)": topics[1],
-#     "Stakeholder’s profit": topics[2],
-#     "Regulation changes(environment)": topics[3],
-#     "Legal Risks": topics[4],
-#     "Financial condition risks ": topics[5],
-#     " Potential/Ongoing Lawsuits": topics[6],
-#     "market Competition risks": topics[7],
-#     "**Labor cost ": topics[8],
-#     " New product introduction risks ": topics[9],
-#     "**Accounting,  +Restructuring risks ": topics[10],
-#     "**Management": topics[11],
-#     " Potential defects in products": topics[12],
-#     "**Investment": topics[13],
-#     "Human resource risks": topics[13],
-#     "Volatile stock price risks": topics[14],
-#     "Merger & Acquisition risks": topics[15],
-#     " +Industry is cyclical": topics[16],
-#     " **Postpone ":  topics[17],
-#     " +Infrastructure risks": topics[18],
-#     "+Suppliers risks +Downstream risks": topics[19],
-#     "license Intellectual property risks": topics[20],
-#     "+Licensing related risks' ": topics[21],
-#     "+ Competition risks ": topics[22],
-#     "*Potential/Ongoing Lawsuits*": topics[23],
-#     "Regulation changes": topics[24],
-#     "Credit risks": topics[25],
-#     "covenant Funding risks ": topics[26],
-#     "International risks": topics[27],
-#     #     "Insurance" : topics[28],
-#     #     "Catastrophes" : topics[29]
-# }
-
-
-
-
-# for topic in topics:
-#     print(topic)
-#     estimator_predict_string(topic)
-
+    return terms, order_centroids, model,
+    truek, model_time, kmeans_score, silhouette_score
 
 
 def generate_model():
-
-    num_docs = int(input("How many docs to process? (-1 for entire folder) : "))
+    num_docs = int(input(
+        "How many docs to process? (-1 for entire folder) : "))
     if num_docs < 2 and num_docs != -1:
         print('More documents please!')
         generate_model()
     doclist, names = make_pipeline(num_docs, istesting=False)
-
-
     tokens = transform_tokens(doclist)
-    filtered_tokens, names_list = transform_filtered(tokens, doclist, names)
+    filtered_tokens, _ = transform_filtered(tokens, doclist, names)
     stemmed = transform_stemming(filtered_tokens)
-
-
-    largest_ngram = 15
     smallest_ngram = 1
     largest_ngram = len(max(tokens, key=len))
-    sparseMatrix, vectorizer = transform_vectorize(stemmed, smallest_ngram, largest_ngram)
-
-
-    terms, order_centroids, model, truek, model_time, kmeans_score, silhouette_score = estimator_cluster(sparseMatrix, vectorizer)
+    sparseMatrix, vectorizer = transform_vectorize(
+        stemmed, smallest_ngram, largest_ngram)
+    estimator_cluster(sparseMatrix, vectorizer)
 
 generate_model()
